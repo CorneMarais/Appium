d06ecdc4293012202fbb3ebd6d3829acbd907049
Corne Marais <cmarais@DVTL100760.local>
2023-03-14 00:13:00 +0200

    First

---
+++ src/test/java/FirstAndroidTest.java
@@ -0,0 +1,21 @@
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+
+public class FirstAndroidTest {
+
+    AppiumDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platform","Android");
+        caps.setCapability("automationName","UiAutomator2");
+        caps
+
+
+
+
+    }
+
+}
